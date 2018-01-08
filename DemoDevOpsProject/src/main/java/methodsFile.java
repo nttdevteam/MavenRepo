d5ab5b35fe021adf33b06ca7d780966d957ae58d
Onkarnath <onkarnath.rai@nttdata.com>
2018-01-08 17:20:21 +0530

    committing project in remote repository

---
+++ DemoDevOpsProject/src/main/java/methodsFile.java
@@ -0,0 +1,24 @@
+public class methodsFile{
+ int c=0;
+public  void addition(int a, int b){
+ c=a+b;
+
+}
+public  void subtraction(int a, int b){
+
+	c=a-b;
+
+}
+public  void multiplication(int a, int b){
+
+	c=a*b;
+
+}
+public  void division(int a, int b){
+
+	c=a/b;
+
+	}
+
+}
+
